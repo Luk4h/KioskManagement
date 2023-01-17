80260562bb230027b20c42daff06bc8ecc58bc12
Luiz Gustavo Marcondes <lukah-oficial@hotmail.com>
2023-01-17 20:23:13 -0300

    Added query path

--- Backend/src/controllers/kiosk/kiosk.controller.ts
+++ Backend/src/controllers/kiosk/kiosk.controller.ts
@@ -3,7 +3,7 @@ import Controller from '../../utils/interfaces/controller';
 import KioskModel from '../../models/kiosk';
 import HttpException from '../../utils/exceptions/exceptions';
 
-class PostController implements Controller {
+class KioskController implements Controller {
   public path = '/kiosks';
   public router: Router = Router();
 
@@ -12,6 +12,7 @@ class PostController implements Controller {
   }
 
   private initialiseRoutes() {
+    this.router.get(`${this.path}`, this.query);
     this.router.post(`${this.path}`, this.create);
   }
 
@@ -40,9 +41,22 @@ class PostController implements Controller {
       });
       response.status(201).json({kiosk});
     } catch (error) {
-      next(new HttpException(400, 'Cannot create post'));
+      next(new HttpException(400, 'Cannot create kiosk'));
+    }
+  };
+
+  private query = async (
+    _request: Request,
+    response: Response,
+    next: NextFunction
+  ) => {
+    try {
+      const kiosks = await KioskModel.find().exec();
+      response.status(200).json({kiosks});
+    } catch (error) {
+      next(new HttpException(400, 'Cannot query kiosks'));
     }
   };
 }
 
-export default PostController;
+export default KioskController;
