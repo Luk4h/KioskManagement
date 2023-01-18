1e42e308c521d51fab2d6585db8be49ff89ca87b
Luiz Gustavo Marcondes <lukah-oficial@hotmail.com>
2023-01-17 21:15:59 -0300

    Added dotenv

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,7 +1,12 @@
 import App from './app';
+// eslint-disable-next-line node/no-unpublished-import
+import * as dotenv from 'dotenv';
 
+dotenv.config();
 // TODO: Validade .env file
 
-const app = new App(3000);
+const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+
+const app = new App(port);
 
 app.listen();
