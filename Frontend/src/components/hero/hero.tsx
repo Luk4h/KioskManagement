b88d7101344a731bab6e3350b1a777b98745c1db
Luiz Gustavo Marcondes <lukah-oficial@hotmail.com>
2023-01-18 11:38:00 -0300

    changed to export default and removed pageLink component

--- Frontend/src/components/hero/hero.tsx
+++ Frontend/src/components/hero/hero.tsx
@@ -1,13 +1,13 @@
-import { PageLink } from "./pageLink"
-
-export const Hero = () => {
+const Hero = () => {
 
     return (
-        <section id="Hero" className="flex justify-between w-full p-6 border-b">
-            <div className="Branding font-bold font-display w-full flex flex-col justify-center text-3xl">
+        <section id="Hero" className="flex justify-between w-full p-6 border-b  bg-slate-100 dark:bg-slate-900 dark:border-slate-600">
+            <div className="Branding font-bold font-display w-full flex flex-col justify-center text-3xl dark:text-slate-400">
                 🏬 Kiosk Management
             </div>
             <div className="w-1/16"></div>
         </section>
     )
 }
+
+export default Hero;
